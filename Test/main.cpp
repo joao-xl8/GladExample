03c783aaef671104fd72594083871335d445c59d
joao_arnold.py <joaopaulo167@gmail.com>
2023-06-12 12:33:15 -0300

    Initial Commit

---
+++ Test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Test
+//
+//  Created by João Paulo Arnold Barros on 12/06/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
